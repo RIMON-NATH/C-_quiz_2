0461fc838e97a14f46d759dbb8100da2f86d23c2
unknown <rimonnath11@gmail.com>
2021-01-02 00:27:06 +0600

    first commit

---
+++ Library/Library/Form1.cs
@@ -0,0 +1,76 @@
+﻿using Library.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBox1.Text);
+            dynamic result1 = BookController.GetAllBook2(id);
+            if (result1 != null)
+            {
+                dataGridView1.DataSource = result1;
+            }
+            else
+            {
+                MessageBox.Show("No Books found");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var book = new
+            {
+                Id = Convert.ToInt32(textBox2.Text),
+                Name = textBox3.Text,
+                Author = textBox4.Text,
+                Edition =textBox5.Text
+            };
+            var result = BookController.AddFood(book);
+            if (result)
+            {
+                MessageBox.Show("Book Added");
+            }
+            else
+            {
+                MessageBox.Show("Book Couldnt't Added");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var result2 = BookController.GetAllBook1();
+            dataGridView2.DataSource = result2;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Library/Library/Models/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Database
+    {
+        public Books Books { get; set; }
+        public Database()
+        {
+            string connetionString = @"Server=DESKTOP-TQA9GGD\SQLEXPRESS;Database=BMS;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connetionString);
+            Books = new Books(conn);
+        }
+    
+     }
+}
